e10971ad28bc860374b48cdb98694c58e220541e
minhajul islam <mm01882390860@gmail.com>
2025-05-09 08:36:37 +0600

    simple statement project done

---
+++ script/script.js
@@ -0,0 +1,30 @@
+let a, b, c;
+
+a = 10;
+b = 20;
+
+c = a * b;
+
+document.getElementById("statement").innerHTML ="মোট গুণফল"+ c;
+
+
+let num1, num2, total;
+
+num1 = 7;
+
+num2 = 3;
+
+total = num1 - num2;
+
+document.getElementById("statement2").innerHTML ="মোট বিয়োগফল"+ total;
+
+
+let x, y, result;
+
+x = 4;
+
+y = 2;
+
+result = x / y;
+
+document.getElementById("statement3").innerHTML ="মোট ভাগফল"+ result;
